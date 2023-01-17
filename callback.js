539ec1c620da146ef097d510e12fee299a2e1c4f
Rajesh Saini <saini222302@gmail.com>
2023-01-17 11:32:33 +0530

    backend class

---
+++ callback.js
@@ -0,0 +1,11 @@
+function getData(callback) {
+//test will get data from google maps.
+
+         callback("mumbai");
+
+    
+}
+
+function prakash(data) {
+   console.log("prakash got the data:", data);
+}
