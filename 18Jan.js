841e4f078ae7481fecfc4375a087249e41b31c4f
Rajesh Saini <saini222302@gmail.com>
2023-01-18 22:50:46 +0530

    Backend node.js 9.30pm

---
+++ 18Jan.js
@@ -0,0 +1,25 @@
+// console.log("hello word ")
+const http = require("http")  //require is new in build function 
+const server = http.createServer((req, res) => {
+    console.log(req.url)
+    res.setHeader("Content-Type", "text/html")
+    let url = req.url
+    if (url === "/") {
+        res.write("home")
+    } else if (url === "/about") {
+        console.log("we are awesome")
+    } else {
+        res.write("page not found")
+    }
+    //Listener function
+    // console.log("hi")
+    // res.setHeader("Content-Type", "text/html")
+    // res.write("hi")
+    res.write("<html>")
+    res.write("<head><title>Aircampus</title></head>")
+    res.write("<body><h1> Hi Rajesh Saini</h1><input /></body>")
+    res.write(`<body><form action="masseg" method="POST" <input type ="text" name="Rajesh" <button type="submit">send</button>> ></form></body>`)
+    res.write("</html>")
+    res.end()
+})
+server.listen(3000)
